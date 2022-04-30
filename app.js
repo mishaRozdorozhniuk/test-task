9da8bfe0f88ea2e9cbfe191d9d2cebcb5ca99d8f
mishaRozdoroznhuk <misha.roz.2003@gmail.com>
2022-04-30 11:34:20 +0200

    add test task website

---
+++ app.js
@@ -0,0 +1,9 @@
+$('.slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+});
